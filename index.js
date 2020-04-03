10390e6e7fb4fb08c0797c766bb3668f8297c59b
GeorgeSmith2003 <63106507+GeorgeSmith2003@users.noreply.github.com>
2020-04-03 14:07:30 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+//javascript
+
+// allows you to use logic on websites
+
+//variable-storage space
+
+let name = "George";
+let age = "16";
+let school = "St Bedes Catholic High School";
+
+console.log("Hi my name is "+ name)
+console.log("I am "+ age + " years old")
+console.log("I went to " + school)
+
+let numberOne;
+let numberTwo;
+let total;
+
+const addition = (numberOne, numberTwo) => {
+    total = numberOne + numberTwo
+ console.log(total)
+}
+
+addition(20,30);
+addition(30,70);
+addition(40,15);
